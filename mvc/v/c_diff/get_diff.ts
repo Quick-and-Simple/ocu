135485592c2bd12353608f73331b83081968e662
borislav nikolov <vf1100c@gmail.com>
2012-01-20 14:57:12 +0200

    WIP

--- mvc/v/c_diff/get_diff.ts
+++ mvc/v/c_diff/get_diff.ts
@@ -9,9 +9,13 @@
 			%center
 				#{count($ctx->diff_result['files']) ? l('diff_modified_files') : l('diff_no_modified_files')}
 		- if (count($ctx->diff_result['files']))
-			%input.checkall type='checkbox' name='not_used_chck_all'
-				select all
-			%br
+			%center
+				%a href='#' id=checkall
+					select all
+				/
+				%a href='#' id=uncheckall
+					deselect all				
+				%br
 		- foreach($ctx->diff_result{'files'} as $f)
 			%input type='checkbox' name='path[]' value='#{$f}'
 			%code
@@ -23,8 +27,13 @@
 			%input.'btn primary' type='submit' #{count($ctx->diff_result['files']) ? "" : "disabled" }  value=#{l('diff_update')}
 	
 		%script
-			$(function () {
-				$('.checkall').click(function () {
-					$(this).parents('fieldset:eq(0)').find(':checkbox').attr('checked', this.checked);
-				});
+			$('#checkall').click(function () {
+				$.each($.find(':checkbox'), function() { 
+					$(this).attr('checked','');
+				})			
+			});
+			$('#uncheckall').click(function () {
+				$.each($.find(':checkbox'), function() { 
+					$(this).removeAttr('checked');
+				})			
 			});
